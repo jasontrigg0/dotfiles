8d9287894bb5dcab376216ac13c91c5c33424205
jasontrigg0 <jasontrigg0@gmail.com>
2016-02-04 23:50:45 -0500

    Added machine_setup file of bash commands to run on a new computer

---
+++ machine_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt-get install emacs
+sudo apt-get install scid
+sudo apt-get install pgn-extract
+cd ~ && wget -O - "http://www.dropbox.com/download?plat=lnx.x86_64" | tar xz
+sudo apt-get install git
+sudo apt-get install python-dev
+sudo apt-get install libfreetype6-dev
+sudo apt-get install python-pip
+sudo apt-get install python-bs4
+sudo easy_install pandas
+sudo easy_install matplotlib
+sudo easy_install sklearn
+sudo easy_install csvkit
+
+
+sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
+sudo pip install numpy
+sudo pip install scipy
+sudo pip install pandas
+
+sudo pip install xlrd
+sudo apt-get install libmysqlclient-dev
+sudo pip install MySQL-python
+
+sudo easy_install sqlalchemy
+sudo easy_install pymysql
