a2d8f140cc71a88c0c8b06f4d634e2479133d24d
jasontrigg0 <jasontrigg0@gmail.com>
2016-07-24 11:01:53 -0400

    phantom-js

--- machine_setup.sh
+++ machine_setup.sh
@@ -26,3 +26,14 @@ sudo pip install MySQL-python
 
 sudo easy_install sqlalchemy
 sudo easy_install pymysql
+
+#phantomjs
+sudo apt-get install build-essential chrpath libssl-dev libxft-dev
+sudo apt-get install libfreetype6 libfreetype6-dev
+sudo apt-get install libfontconfig1 libfontconfig1-dev
+cd ~
+export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
+wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
+sudo tar xvjf $PHANTOM_JS.tar.bz2
+sudo mv $PHANTOM_JS /usr/local/share
+sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
