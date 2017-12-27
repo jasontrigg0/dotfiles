92b2c50c674a69b5afdc5ac202efc64bcbf3e97b
jasontrigg0 <jasontrigg0@gmail.com>
2017-12-27 03:42:43 -0500

    Cleanup, also comment out some that aren't currently being used

--- machine_setup.sh
+++ machine_setup.sh
@@ -1,51 +1,66 @@
 #!/bin/bash
 
-sudo apt-get install emacs
-sudo apt-get install scid
-sudo apt-get install pgn-extract
-cd ~ && wget -O - "http://www.dropbox.com/download?plat=lnx.x86_64" | tar xz
-sudo apt-get install git
-sudo apt-get install python-dev
-sudo apt-get install libfreetype6-dev
-sudo apt-get install python-pip
-sudo apt-get install python-bs4
-sudo easy_install pandas
-sudo easy_install matplotlib
-sudo easy_install sklearn
-sudo easy_install csvkit
+sudo apt install emacs
+sudo apt install git
+sudo apt install scid
+sudo apt install curl
+sudo apt install pgn-extract
+# cd ~ && wget -O - "http://www.dropbox.com/download?plat=lnx.x86_64" | tar xz
+# sudo apt install python-dev
+# sudo apt install libfreetype6-dev
+
+#python
+sudo apt install python-pip
+sudo apt install python3-pip
+sudo apt install python-bs4
+sudo apt install python3-bs4
+sudo pip install pandas
+sudo pip3 install pandas
+sudo pip install matplotlib
+sudo pip3 install matplotlib
+sudo apt install python-tk
+sudo apt install python3-tk
+sudo pip install sklearn
+sudo pip3 install sklearn
+# sudo easy_install csvkit
 
 #numpy scipy pandas
-sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
-sudo pip install numpy
+# sudo apt install libblas-dev liblapack-dev libatlas-base-dev gfortran
+# sudo pip install numpy
 sudo pip install scipy
-sudo pip install pandas
+sudo pip3 install scipy
 
 sudo pip install xlrd
-sudo apt-get install libmysqlclient-dev
-sudo pip install MySQL-python
+# sudo apt install libmysqlclient-dev
+# sudo pip install MySQL-python
 
-sudo easy_install sqlalchemy
-sudo easy_install pymysql
+# sudo easy_install sqlalchemy
+# sudo easy_install pymysql
 
 sudo pip install diff-highlight #for pretty git diff
 
 #ag
-sudo apt-get install silversearcher-ag
+sudo apt install silversearcher-ag
 
-sudo apt-get install gyazo #easy screenshot sending
-sudo apt-get install i3
-sudo apt-get install jp2a
+#easy screenshot sending
+curl -s https://packagecloud.io/install/repositories/gyazo/gyazo-for-linux/script.deb.sh | sudo bash
+sudo apt install gyazo
+
+sudo apt install i3
+sudo apt install jp2a
+
+sudo apt install screen
 
 #phantomjs
-sudo apt-get install build-essential chrpath libssl-dev libxft-dev
-sudo apt-get install libfreetype6 libfreetype6-dev
-sudo apt-get install libfontconfig1 libfontconfig1-dev
-cd ~
-export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
-wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
-sudo tar xvjf $PHANTOM_JS.tar.bz2
-sudo mv $PHANTOM_JS /usr/local/share
-sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
+# sudo apt install build-essential chrpath libssl-dev libxft-dev
+# sudo apt install libfreetype6 libfreetype6-dev
+# sudo apt install libfontconfig1 libfontconfig1-dev
+# cd ~
+# export PHANTOM_JS="phantomjs-2.1.1-linux-x86_64"
+# wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
+# sudo tar xvjf $PHANTOM_JS.tar.bz2
+# sudo mv $PHANTOM_JS /usr/local/share
+# sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
 
 #earlyoom (kill processes before they take up all your memory)
 #https://github.com/rfjakob/earlyoom
@@ -56,19 +71,34 @@ make
 sudo make install
 
 #noti for desktop and mobile (pushbullet) notifications
-#https://github.com/variadico/noti
-#using tee to redirect to a protected folder
+# https://github.com/variadico/noti
+# using tee to redirect to a protected folder
 curl -L https://github.com/variadico/noti/releases/download/v2.5.0/noti2.5.0.linux-amd64.tar.gz | tar -xOz | sudo tee /usr/bin/noti > /dev/null
 sudo chmod +x /usr/bin/noti
 
 #dex for autostart with i3
 #https://faq.i3wm.org/question/2155/how-can-i-use-autostart-desktop-files-in-i3.1.html
-sudo apt-get install dex
-
-#TODO: gitless install, if it ends up useful http://gitless.com/
+sudo apt install dex
 
 #concat pdfs
-sudo apt-get install pdftk
+sudo apt install pdftk
 
 #https://github.com/mbostock/gistup
 npm install -g gistup
+
+#monitoring
+sudo apt install htop
+sudo apt install iotop
+sudo apt install powertop
+sudo apt install nethogs
+
+#percol
+sudo pip install percol
+
+#watch files and run command when they change
+#https://github.com/clibs/entr
+sudo apt install entr #ls file_to_watch.txt | entr sh -c 'echo "File changed"'
+
+sudo apt install jq #used by weather() command in bashrc
+
+#TODO: gitless install, if it ends up useful http://gitless.com/
